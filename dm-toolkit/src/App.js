5731a70109a6b5cd7aa6365e42844c9092c56717
TheRedLancer <zburnaby918@gmail.com>
2020-06-13 16:58:00 -0700

    add clock

--- dm-toolkit/src/App.js
+++ dm-toolkit/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './HelloWorld';
+import Clock from './Clock';
 
 function App() {
 	return (
 		<div className="App">
+			<Clock />
 			<HelloWorld />
 		</div>
 	);
