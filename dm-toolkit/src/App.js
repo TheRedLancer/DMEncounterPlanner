5a5623ac630cdfce8a7e65c88313e945be85d4ad
TheRedLancer <zburnaby918@gmail.com>
2020-07-01 19:26:45 -0700

    removed clock

--- dm-toolkit/src/App.js
+++ dm-toolkit/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Clock from './Clock.js';
 import SubmitBox from './SubmitBox';
 
 function App() {
 	return (
 		<div className="App">
-			<Clock />
 			<SubmitBox />
 		</div>
 	);
